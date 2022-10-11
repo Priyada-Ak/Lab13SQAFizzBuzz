bbfc4210c41e546bf6e42d8105631c210a4cea75
atsadawutcs <atsadawut.h@kkumail.com>
2022-10-11 18:59:31 +0700

    Create Main when input more than 100

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -2,19 +2,19 @@ package main;
 
 public class FizzBuzz {
 	public static String checkerNumber(int num) {
-		if(num > 0) {
-			if(num % 3 == 0 && num % 5 ==0) {
-				return "FizzBuzz";
-			}else if(num % 3 ==0) {
-				return "Fizz";
-			}else if(num % 5 ==0) {
-				return "Buzz";
-			}else {
-	            return Integer.toString(num); 
-	         }
-		}else {
-			return "The number must be between 0-100!";
-		}
+	    if(num > 0 && num < 101) {
+            if (num % 3 == 0 && num % 5 == 0) {
+                return "FizzBuzz"; 
+             }else if (num % 5 == 0) {
+                return "Buzz"; 
+             }else if (num % 3 == 0) {
+                return "Fizz"; 
+             }else {
+                return Integer.toString(num); 
+             }
+       }else {
+           return "The number must be between 0-100!";
+       }
 		
     }
 }
