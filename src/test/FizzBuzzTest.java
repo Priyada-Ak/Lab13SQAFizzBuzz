d2460dd70293a9210340448110a971671b15a435
Priyada Akama <mut.priyada@gmail.com>
2022-10-11 18:24:27 +0700

    Create Test when input 3

---
+++ src/test/FizzBuzzTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import main.FizzBuzz;
+
+class FizzBuzzTest {
+
+	FizzBuzz fizzbuzz;
+	
+	@DisplayName("Test : Fizz")
+	@Test
+	void testFizz() {
+		String number = FizzBuzz.checkerNumber(3);
+		assertEquals("Fizz", number);
+	}
+}
